53e43dec818d99d2738ed7b07f0764238b8a1d78
Jörn Zaefferer <joern.zaefferer@gmail.com>
2017-11-21 18:41:13 +0100

    add cors headers

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const app = express()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use('/v1/pets', pets)
 
 // basic information - also used as a health check
